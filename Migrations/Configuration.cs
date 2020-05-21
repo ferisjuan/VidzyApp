fe350eb8fb4964e24583019a3bb577da68fde9cc
ferisjuan <feris.juan@gmail.com>
2020-05-21 12:21:18 -0500

    Enable Migrations

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EFCourse.Vidzy.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EFCourse.Vidzy.VidzyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EFCourse.Vidzy.VidzyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
