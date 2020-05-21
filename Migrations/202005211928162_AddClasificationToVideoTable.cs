34aaeb3104246cbb9fe186d61c55bcbf92d5f323
ferisjuan <feris.juan@gmail.com>
2020-05-21 14:30:21 -0500

    Added Calsification To Video And Its Migration

---
+++ Migrations/202005211928162_AddClasificationToVideoTable.cs
@@ -0,0 +1,18 @@
+﻿namespace EFCourse.Vidzy.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddClasificationToVideoTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Videos", "Clasification", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Videos", "Clasification");
+        }
+    }
+}
--- Video.cs
+++ Video.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace EFCourse.Vidzy
 {
+    enum Clasification 
+    {
+        Silver = 1,
+        Gold = 2,
+        Platinum = 3
+    }
     class Video
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
         public Genre Genres { get; set; }
+        public Clasification Clasification { get; set; }
     }
 }
