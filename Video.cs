eb73290e506a0e9472b53268cf03c6519147f294
ferisjuan <feris.juan@gmail.com>
2020-05-21 12:01:51 -0500

    Added Video And Genre Models

---
+++ Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCourse.Vidzy
+{
+    class Video
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public IList<Genre> Genres { get; set; }
+    }
+}
