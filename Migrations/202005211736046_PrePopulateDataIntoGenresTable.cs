ac1d5e6f091a5594831826e1e746c1df22438e57
ferisjuan <feris.juan@gmail.com>
2020-05-21 12:36:41 -0500

    Add-Migration PrePopulateDataIntoGenresTable

---
+++ Migrations/202005211736046_PrePopulateDataIntoGenresTable.cs
@@ -0,0 +1,19 @@
+﻿namespace EFCourse.Vidzy.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PrePopulateDataIntoGenresTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Genre) ('Action')");
+            Sql("INSERT INTO Genres (Genre) ('Drama')");
+            Sql("INSERT INTO Genres (Genre) ('Fiction')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
